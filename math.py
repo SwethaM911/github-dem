5e5eb9a688019a253ca3749ab23a5eb121233040
SwethaM911 <87117903+SwethaM911@users.noreply.github.com>
2021-07-08 01:59:05 -0700

    Add files via upload

---
+++ math.py
@@ -0,0 +1,17 @@
+#add implementations
+def add(x,y):
+      return x+y
+
+def subtract(x,y):
+      return x-y
+
+#multiply fun header
+def multiply(x,y):
+      return x*y        #implementation on bug456
+
+def divide(x,y):
+      if y==0:
+         return DIVIDE_BY_0_ERROR
+      else:
+         return x/y
+
